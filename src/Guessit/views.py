d3505ce5c041270290c82ce37b3e9ac04adce1f5
vinothini <vinothinicbe@gmail.com>
2014-09-22 18:21:59 +0530

    some fixes

--- src/Guessit/views.py
+++ src/Guessit/views.py
@@ -19,7 +19,6 @@ def display_question(request):
     global question_id
     context = RequestContext(request)
     try:
-        #quesno_id = {'id':question_id}
         song_data = Songdata.objects.get(song_id=question_id)
         total_ques = Songdata.objects.all().count()
     except Songdata.DoesNotExist:
@@ -29,7 +28,7 @@ def display_question(request):
         form = Song_form(request.POST)
         if form.is_valid():
             user_answer = form.cleaned_data['answer']
-            if song_data.song_name.strip() == user_answer.strip():
+            if song_data.song_name.strip().lower() == user_answer.strip().lower():
                 if int(question_id) < int(total_ques):
                     question_id = question_id+1
                     print question_id
@@ -37,9 +36,8 @@ def display_question(request):
                     print song_data.lyrics
                     form = Song_form()
                     return render_to_response('display_question.html', {'form':form,'song_data':song_data,'quesno_id':question_id,'total_ques':total_ques},context)
-                    #return HttpResponseRedirect("/SongQuiz/question/%s/" % (int(question_id)+1))
                 else:
-                    return render_to_response('results.html')
+                    return render_to_response('results.html',context)
             else:
                 return HttpResponse("Your answer is wrong")
         else:
--- src/SongQuiz/settings.py
+++ src/SongQuiz/settings.py
@@ -87,7 +87,10 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,'static','templates'),
     )
+print os.path.join(BASE_DIR,'static','templates')
+
 if DEBUG:
     STATICFILES_DIRS  = (
         os.path.join(BASE_DIR,'static','static'),
         )
+print os.path.join(BASE_DIR,'static','static')
